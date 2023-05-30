8413c84e3b03ba4347c01a15a35e1c5c8b9a9b93
Diego Rubí Salas <diegoalru@gmail.com>
2023-05-30 01:21:58 -0600

    Pantalla de Ajustes agregada.

--- app/src/main/java/com/dars/forecastify/MainActivity.kt
+++ app/src/main/java/com/dars/forecastify/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.dars.forecastify
 
 import android.Manifest
+import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.preference.PreferenceManager
 import com.dars.forecastify.controllers.WeatherController
+import com.dars.forecastify.databinding.ActivityMainBinding
 import com.dars.forecastify.models.WeatherData
 import com.dars.forecastify.utils.Language
 import com.dars.forecastify.utils.Mode
@@ -20,72 +25,75 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var unit: Unit
 
     private val locationPermissionRequestCode = 100
-
-    private lateinit var cityCountry: TextView
-    private lateinit var actualDate: TextView
-    private lateinit var latitude: TextView
-    private lateinit var longitude: TextView
-    private lateinit var mainDescription: TextView
-    private lateinit var mainTemperature: TextView
-    private lateinit var feelsLike: TextView
-    private lateinit var minTemperature: TextView
-    private lateinit var maxTemperature: TextView
-    private lateinit var humidity: TextView
-    private lateinit var windiness: TextView
-    private lateinit var pressure: TextView
-    private lateinit var sunrise: TextView
-    private lateinit var sunset: TextView
-
-    private lateinit var mainContent: View
-    private lateinit var loadingContent: View
-
-    private val coroutineScope = CoroutineScope(Dispatchers.Main)
-
     private val weatherController = WeatherController()
-    private lateinit var mode: Mode
-    private lateinit var unit: Unit
-    private lateinit var lang: Language
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        mainContent = findViewById(R.id.main_container)
-        loadingContent = findViewById(R.id.loading_container)
-
-        cityCountry = findViewById(R.id.address)
-        actualDate = findViewById(R.id.updated_at)
-        latitude = findViewById(R.id.latitude)
-        longitude = findViewById(R.id.longitude)
-        mainDescription = findViewById(R.id.status)
-        mainTemperature = findViewById(R.id.temp)
-        feelsLike = findViewById(R.id.feels_like)
-        minTemperature = findViewById(R.id.temp_min)
-        maxTemperature = findViewById(R.id.temp_max)
-        sunrise = findViewById(R.id.sunrise)
-        sunset = findViewById(R.id.sunset)
-        windiness = findViewById(R.id.wind)
-        pressure = findViewById(R.id.pressure)
-        humidity = findViewById(R.id.humidity)
-
-        mode = Mode.JSON
-        unit = Unit.METRIC
-        lang = Language.ES
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
 
         checkLocatePermission()
 
         // Iniciamos la aplicación con la ubicación actual
-        refreshWeather(mainContent)
+        refreshWeather()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Registrar el listener de cambios en las preferencias
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+
+        val unitValue = sharedPreferences.getString("unit", "metric")
+        updatePreferences(unitValue!!)
+        refreshWeather()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        // Anular el registro del listener de cambios en las preferencias
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                val intentSettings = Intent(this, SettingsActivity::class.java)
+                startActivity(intentSettings)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if (key == "unit") {
+            val unitValue = sharedPreferences?.getString("unit", "metric")
+            updatePreferences(unitValue!!)
+            refreshWeather()
+        }
     }
 
     private fun checkLocatePermission() {
         if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
+                this, Manifest.permission.ACCESS_FINE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             // El permiso no ha sido concedido, solicitarlo
@@ -97,12 +105,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun refreshWeather(view: View) {
+    fun refreshWeatherEvent(view: View) {
+        refreshWeather()
+    }
+
+    private fun refreshWeather() {
         coroutineScope.launch {
             changeContent(true)
 
+            // Obtener idioma del sistema
+            val lang = when (Locale.getDefault().language) {
+                "es" -> Language.ES
+                "en" -> Language.EN
+                else -> Language.EN
+            }
+
             try {
-                val weatherData = weatherController.getWeatherData(this@MainActivity, mode.value, unit.value, lang.value)
+                val weatherData = weatherController.getWeatherData(
+                    this@MainActivity, Mode.JSON.value, unit.value, lang.value
+                )
 
                 if (weatherData != null) {
                     updateUI(weatherData)
@@ -111,51 +132,83 @@ class MainActivity : AppCompatActivity() {
                         .show()
                 }
                 changeContent(false)
+            } catch (e: IllegalArgumentException) {
+                Toast.makeText(this@MainActivity, "${e.message}", Toast.LENGTH_LONG).show()
             } catch (e: Exception) {
                 Toast.makeText(this@MainActivity, "${e.message}", Toast.LENGTH_LONG).show()
                 // Esperamos 5 segundos para volver a intentar
                 delay(5000)
 
                 checkLocatePermission()
-                refreshWeather(view)
+                refreshWeather()
             }
         }
     }
 
     private fun updateUI(weatherData: WeatherData) {
         with(weatherData) {
-            cityCountry.text = getString(R.string.city_country, name, sys.country)
-            actualDate.text =
-                getString(R.string.actual_date, weatherController.convertUnixDate(dt, true))
-            latitude.text = getString(R.string.latitude, coord.latitude.toString())
-            longitude.text = getString(R.string.longitude, coord.longitude.toString())
-            mainDescription.text =
-                getString(R.string.main_description, weather[0].description.replaceFirstChar {
-                    if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString()
-                })
-            mainTemperature.text = getString(R.string.main_temperature, main.temperature.toString())
-            feelsLike.text = getString(R.string.feels_like, main.feelsLike.toString())
-            minTemperature.text =
-                getString(R.string.min_temperature, main.temperatureMin.toString())
-            maxTemperature.text =
-                getString(R.string.max_temperature, main.temperatureMax.toString())
-            sunrise.text =
-                getString(R.string.sunrise, weatherController.convertUnixDate(sys.sunrise, false))
-            sunset.text =
-                getString(R.string.sunset, weatherController.convertUnixDate(sys.sunset, false))
-            windiness.text = getString(R.string.wind, wind.speed.toString())
-            pressure.text = getString(R.string.pressure, main.pressure.toString())
-            humidity.text = getString(R.string.humidity, main.humidity.toString())
+
+            val unitOfMeasure = when (unit) {
+                Unit.STANDARD -> "°K"
+                Unit.METRIC -> "°C"
+                Unit.IMPERIAL -> "°F"
+            }
+
+            binding.apply {
+                cityCountry.text = getString(R.string.city_country, name, sys.country)
+                updatedAt.text =
+                    getString(R.string.actual_date, weatherController.convertUnixDate(dt, true))
+                latitude.text = getString(R.string.latitude, coord.latitude.toString())
+                longitude.text = getString(R.string.longitude, coord.longitude.toString())
+                mainDescription.text =
+                    getString(R.string.main_description, weather[0].description.replaceFirstChar {
+                        if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString()
+                    })
+                temp.text = getString(
+                    R.string.main_temperature,
+                    "${main.temperature} $unitOfMeasure"
+                )
+                feelsLike.text =
+                    getString(R.string.feels_like, "${main.feelsLike} $unitOfMeasure")
+                tempMin.text = getString(
+                    R.string.min_temperature,
+                    "${main.temperatureMin} $unitOfMeasure"
+                )
+                tempMax.text = getString(
+                    R.string.max_temperature,
+                    "${main.temperatureMax} $unitOfMeasure"
+                )
+                txtSunrise.text = getString(
+                    R.string.sunrise,
+                    weatherController.convertUnixDate(sys.sunrise, false)
+                )
+                txtSunset.text =
+                    getString(R.string.sunset, weatherController.convertUnixDate(sys.sunset, false))
+                txtWind.text = getString(R.string.wind, wind.speed.toString())
+                txtPressure.text = getString(R.string.pressure, main.pressure.toString())
+                txtHumidity.text = getString(R.string.humidity, main.humidity.toString())
+            }
         }
     }
 
     private fun changeContent(isLoading: Boolean) {
-        if (isLoading) {
-            mainContent.visibility = View.GONE
-            loadingContent.visibility = View.VISIBLE
-        } else {
-            mainContent.visibility = View.VISIBLE
-            loadingContent.visibility = View.GONE
+        binding.apply {
+            if (isLoading) {
+                mainContainer.visibility = View.GONE
+                loadingContainer.visibility = View.VISIBLE
+            } else {
+                mainContainer.visibility = View.VISIBLE
+                loadingContainer.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun updatePreferences(unitValue: String) {
+        unit = when (unitValue) {
+            "standard" -> Unit.STANDARD
+            "metric" -> Unit.METRIC
+            "imperial" -> Unit.IMPERIAL
+            else -> Unit.METRIC
         }
     }
 }
